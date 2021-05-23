db89d20be4d52ce4b770c7798310089a6918c006
tuananhpta19 <tuananh.pta19@gmail.com>
2021-05-24 01:29:06 +0700

    fix format time

--- ulti/formattime.js
+++ ulti/formattime.js
@@ -1,25 +1,21 @@
-
-
-
 const moment = require('moment');
-module.exports = {
-    formatTime: (d) => {
-        d = Number(d);
-        var h = Math.floor(d / 3600);
-        var m = Math.floor(d % 3600 / 60);
-        var s = Math.floor(d % 3600 % 60);
-        var st = '';
-        if (h != 0) {
-            if (h <= 9) st += '0' + h + ':';
-            else st += h + ':';
-        }
-        if (m <= 9) st += '0' + m + ':';
-        else st += m + ':';
-        if (s <= 9) st += '0' + s;
-        else st += s;
-        return st;
-    },
-    formatTimeComment : (time) => {
-        return moment(time).locale("vi").startOf("seconds").fromNow()
+let formatTimeComment = (time) => {
+    return moment(time).locale("vi").startOf("seconds").fromNow()
+}
+let formatTime = (d) => {
+    d = Number(d);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+    var st = '';
+    if (h != 0) {
+        if (h <= 9) st += '0' + h + ':';
+        else st += h + ':';
     }
-};
+    if (m <= 9) st += '0' + m + ':';
+    else st += m + ':';
+    if (s <= 9) st += '0' + s;
+    else st += s;
+    return st;
+}
+module.exports = {formatTimeComment, formatTime}
