66fc644921b021e76b2787eca2c617267bb089e5
tuananhpta19 <tuananh.pta19@gmail.com>
2021-05-24 01:30:44 +0700

    fix format code

--- ulti/formattime.js
+++ ulti/formattime.js
@@ -1,21 +1,5 @@
 const moment = require('moment');
-let formatTimeComment = (time) => {
+let formatTime = (time) => {
     return moment(time).locale("vi").startOf("seconds").fromNow()
 }
-let formatTime = (d) => {
-    d = Number(d);
-    var h = Math.floor(d / 3600);
-    var m = Math.floor(d % 3600 / 60);
-    var s = Math.floor(d % 3600 % 60);
-    var st = '';
-    if (h != 0) {
-        if (h <= 9) st += '0' + h + ':';
-        else st += h + ':';
-    }
-    if (m <= 9) st += '0' + m + ':';
-    else st += m + ':';
-    if (s <= 9) st += '0' + s;
-    else st += s;
-    return st;
-}
-module.exports = {formatTimeComment, formatTime}
+module.exports = formatTime
